2a46ab72af4e4b4ad3e5182fd91b5094ed7a6364
Jordi <git@jordimenheer.nl>
2012-10-04 11:24:28 +0200

    Main comments

--- src/nl/hro/minor/android/lettergame/jjs/Main.java
+++ src/nl/hro/minor/android/lettergame/jjs/Main.java
@@ -17,24 +17,22 @@ public class Main extends Activity implements android.view.View.OnClickListener{
         ContextHolder ch = ContextHolder.getInstance();
         ch.setContext(this);
         
+        // Get the startbutton and attack click listener
         View aboutButton = findViewById(R.id.btn_start);
         aboutButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-		switch(v.getId()){
+		// Switch over possible incoming views
+    	switch(v.getId()){
 			case R.id.btn_start:
+				// Start game if clicked on the Startbutton
 				Intent i = new Intent(this, Game.class);
 				startActivity(i);
 			break;
 		}
 		
 	}
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+    
 }
