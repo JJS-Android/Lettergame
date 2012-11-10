7335fe014d32bb6324728b9010970abd5a08cde0
sanderpott <beedeeboy@gmail.com>
2012-11-10 18:32:51 +0100

    Cleaned up ZdV + fixed thread issues
    Cleaned up lots of tabs and extra returns/spaces in Zoek-de-verschillen
    (ZdV)
    Fixed thread issues trying to invalidate a view in another thread
    Passed x,y to clickHandler instead of MotionEvent as values changed
    inside thread (!?)
    Threw out all comments from bitmapCompare
    Removed MinigamesActivity + xml as it was redundant

--- src/nl/hro/minor/android/lettergame/jjs/differences/ClickHandler.java
+++ src/nl/hro/minor/android/lettergame/jjs/differences/ClickHandler.java
@@ -1,5 +1,6 @@
 package nl.hro.minor.android.lettergame.jjs.differences;
 
+import nl.hro.minor.android.lettergame.jjs.ContextHolder;
 import nl.hro.minor.android.lettergame.jjs.R;
 
 import java.util.ArrayList;
@@ -9,128 +10,100 @@ import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class ClickHandler {
-	
-private Bitmap _bitmap;
-private contextHolder _ch =  contextHolder.getInstance();
-private boolean returnValue = false;
-
-private ArrayList<Point> _redPoints = new ArrayList<Point>();	
-	
-	public void setBitmap(Bitmap bitmap)
-	{
-		_bitmap = bitmap;
-	}
-	
-	public boolean handleClick(MotionEvent event)
-	{
-    	ImageView iv = (ImageView) _ch.getContext().findViewById(R.id.clickAreaView);
-		_bitmap = ((BitmapDrawable)iv.getDrawable()).getBitmap();
-		
-		int x = (int)event.getX();
-        int y = (int)event.getY();
+    
+    private Bitmap _bitmap;
+    private contextHolder _ch =  contextHolder.getInstance();
+    private boolean _returnValue = false;
+    
+    private ArrayList<Point> _redPoints = new ArrayList<Point>();
+    
+    public ClickHandler() {
+        ImageView iv = (ImageView) _ch.getContext().findViewById(R.id.clickAreaView);
+        _bitmap = ((BitmapDrawable)iv.getDrawable()).getBitmap();
+    }
+    
+    public void setBitmap(Bitmap bitmap) {
+        _bitmap = bitmap;
+    }
+    
+    public boolean handleClick(int x, int y) {
+        if (checkIfRed(x, y)) {
+            Toast.makeText(_ch.getContext(), "Raak!", Toast.LENGTH_SHORT).show();
+            _redPoints.add(new Point(x, y));
+            loopThroughPoints();
+            Game1.clickedRightCount++;
+            if (Game1.clickedRightCount == 3) {
+                _returnValue = true;
+                return true;
+            }
+        } else {
+            //TODO: stuff to punish for not guessing correctly
+        }
         
-        if(checkIfRed(x, y) == true)
-		{
-        	Log.d("red", "red");
-        	contextHolder ch = contextHolder.getInstance();
-        	Toast.makeText(ch.getContext(), "Raak!", Toast.LENGTH_SHORT).show();
-		 _redPoints.add(new Point(x, y));
-		 loopThroughPoints();
-		 Game1.clickedRightCount++;
-		 if (Game1.clickedRightCount == 3)
-		 {
-			 returnValue = true;
-			 return true;
-		 }
-		}
-		else
-		{
-			Log.d("not red", "not red");
-			//do stuff to punish for nog guessing correctly
-		}
+        _returnValue = false;
+        return false;
+    }
+    
+    private boolean checkIfRed(int x, int y) {
+        if (isPointInImage(x,y)) {
+            int pixel = _bitmap.getPixel(x,y);
+            if(pixel == Color.RED){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    private void loopThroughPoints() {
+        for(int i = 0; i < _redPoints.size(); i++ ) {
+            Point point = _redPoints.get(i);
+            checkforRedpixel(point.x, point.y);
+        }
+    }
+    
+    private void checkforRedpixel(int x, int y) {
+        ArrayList<Point> points = new ArrayList<Point>();
         
-    	_ch.getContext().findViewById(R.id.clickAreaView).invalidate();
-    	
-    	returnValue = false;
-    	return false;
-		
-	}
-	
-	public boolean getLastReturnValue(){
-		return returnValue;
-	}
-	
-    private void checkforRedpixel(int x, int y)
-    {
-    	
-    	ArrayList<Point> points = new ArrayList<Point>();
-    	
-    	if (isPointInImage(x+1,y)) {
-    		points.add(new Point(x+1,y));
-    	}
-    	if (isPointInImage(x-2,y)) {
-    		points.add(new Point(x-2,y));
-    	}
-    	if (isPointInImage(x,y+1)) {
-    		points.add(new Point(x,y+1));
-    	}
-    	if (isPointInImage(x,y-1)) {
-    		points.add(new Point(x,y-1));
-    	}
-    	
-    	for (Point point:points )
-    	{
-    		if(checkIfRed(point.x, point.y)==true)
-    		{
-    		 _redPoints.add(point);
-    		 reColorPixel(point);
-    		}
-    	}
+        if (isPointInImage(x+1,y)) {
+            points.add(new Point(x+1,y));
+        }
+        if (isPointInImage(x-2,y)) {
+            points.add(new Point(x-2,y));
+        }
+        if (isPointInImage(x,y+1)) {
+            points.add(new Point(x,y+1));
+        }
+        if (isPointInImage(x,y-1)) {
+            points.add(new Point(x,y-1));
+        }
+        
+        for (Point point:points ) {
+            if (checkIfRed(point.x, point.y)) {
+                //_redPoints.add(point); // this caused an infinite loop, what was its purpose?
+                reColorPixel(point);
+            }
+        }
     }
     
     private boolean isPointInImage(int x, int y) {
-    	if (x > 0 && x < _bitmap.getWidth() && y > 0 && y < _bitmap.getHeight()) {
-    		return true;
-    	} else {
-    		return false;
-    	}
+        if (x > 0 && x < _bitmap.getWidth() && y > 0 && y < _bitmap.getHeight()) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
-    private boolean checkIfRed(int x, int y)
-    {
-		if (isPointInImage(x,y)) {
-			int pixel = _bitmap.getPixel(x,y);
-			if(pixel== Color.RED){
-	           return true;
-	         }
-			else
-			{
-				return false;
-			}
-		}
-		return false;
+    private void reColorPixel(Point point) {
+        _bitmap.setPixel(point.x, point.y, Color.BLUE);
     }
     
-    private void loopThroughPoints()
-    {
-    	for(int i = 0; i < _redPoints.size(); i++ )
-    	{
-    		Point point = _redPoints.get(i);
-    		checkforRedpixel(point.x, point.y);
-    	}
+    public boolean getLastReturnValue() {
+        return _returnValue;
     }
-    
-    private void reColorPixel(Point point)
-    {
-    	_bitmap.setPixel(point.x, point.y, Color.BLUE);
-    }
-
-    
-
-
 }
--- src/nl/hro/minor/android/lettergame/jjs/differences/Game1.java
+++ src/nl/hro/minor/android/lettergame/jjs/differences/Game1.java
@@ -19,161 +19,153 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 public class Game1 extends Activity implements OnTouchListener {
-	
-	private ClickHandler _clikhandler = new ClickHandler();
-	private contextHolder ch;
-	private levelManager lvlmngr = new levelManager();
-	private ImageView clickArea;
-	private ImageView imageView1;
-	private ImageView imageView2;
-	private ProgressBar progessBar;
-	private CountDownTimer countDown;
-	
-	public static int clickedRightCount;
-	
-	private int currentLevel = 1;
-	
-	private int _timeLeft;
-	
-	public static final int playTime = 60;
-	public static int _totalScore = 0;
     
-	/** Called when the activity is first created. */
+    private ClickHandler _clickhandler;
+    private contextHolder _ch;
+    private levelManager _lvlmngr = new levelManager();
+    private ImageView _clickArea;
+    private ImageView _imageView1;
+    private ImageView _imageView2;
+    private ProgressBar _progessBar;
+    private CountDownTimer _countDown;
+    private ProgressDialog dialog;
+    
+    public static int clickedRightCount;
+    
+    private int _currentLevel = 1;
+    
+    private int _timeLeft;
+    
+    public static final int playTime = 60;
+    public static int _totalScore = 0;
+    
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.playfield);
         
         // Set the current context (this) in the 'singleton' contextHolder.
-        ch = contextHolder.getInstance();
-        ch.setContext(this);
+        _ch = contextHolder.getInstance();
+        _ch.setContext(this);
         /*
         lvlmngr.getNewLevel();
         */
         
-        progessBar = (ProgressBar) findViewById(R.id.progress_bar);
-        
-        clickArea = (ImageView) findViewById(R.id.clickAreaView);
-        imageView1 = (ImageView) findViewById(R.id.imageView1);
-        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        _progessBar = (ProgressBar) findViewById(R.id.progress_bar);
+        _clickArea = (ImageView) findViewById(R.id.clickAreaView);
+        _imageView1 = (ImageView) findViewById(R.id.imageView1);
+        _imageView2 = (ImageView) findViewById(R.id.imageView2);
 
-		startLevel();
-		imageView1.setOnTouchListener(this);
-		imageView2.setOnTouchListener(this);
+        loadImgs();
+        _clickhandler = new ClickHandler();
+        startLevel();
+        _imageView1.setOnTouchListener(this);
+        _imageView2.setOnTouchListener(this);
         
     }
-
-    private ProgressDialog dialog;
     
     public boolean onTouch(View v, MotionEvent event) {
-
-        
         int action = event.getAction();
-        
-        if (action == MotionEvent.ACTION_DOWN)
-        {
-        	
-        	// Show loading dialog
-    		dialog = new ProgressDialog(this);
+        if (action == MotionEvent.ACTION_DOWN) {
+            // Show loading dialog
+            dialog = new ProgressDialog(this);
             dialog.setMessage("Controleren...");
             dialog.show();
             
             // Create handler
-            final Handler h = new Handler(){
-    		    public void handleMessage(Message msg) {
-    		    	// If thread is finished a message will be send to this method
-    		    	
-    		    	if (_clikhandler.getLastReturnValue()) {
-    	        		currentLevel++;
-    	        		clickedRightCount = 0;
-    	        		countDown.cancel();
-    	        		Intent i = new Intent(ch.getContext(), Score.class);
-    	        		i.putExtra("timeLeft", (_timeLeft/1000));
-    	        		if (currentLevel > 3) {
-    		        		i.putExtra("isFinished", true);
-    		        		startActivity(i);
-    	        		} else {
-    		        		i.putExtra("isFinished", false);
-    		        		startActivity(i);
-    		        		startLevel();
-    	        		}
-    	        	}
-    		    	
-    		    	Log.w("PixelCheck", "Done, close loading message");
-    		    	dialog.dismiss();
-    		    	
-    		    } 
-    		};
-    		
-    		final MotionEvent fEvent = event;
-    		
-    		// Run clickHandler in a seperate thread
-    		Thread t = new Thread() {
-    	        public void run() {
-    	          try {
-    	        	  
-    	        	//Looper.prepare(); // No idea why, just have to use it
-    	        	Looper.prepareMainLooper();
-    				
-    	        	//pass event into clickhandler for checks
-    	          	_clikhandler.handleClick(fEvent);
-    				
-    				// Send empty message to the handler
-    				h.sendEmptyMessage(0);
-    	
-    	          }catch (Exception e) {
-    	        	  Log.w("Clickhandler", "PixelCheck Failed: " + e.getMessage());
-    	          }
-    	        }
+            final Handler h = new Handler() {
+                public void handleMessage(Message msg) {
+                    // If thread is finished a message will be send to this method
+                    if (_clickhandler.getLastReturnValue()) {
+                        findViewById(R.id.clickAreaView).invalidate();
+                        _currentLevel++;
+                        clickedRightCount = 0;
+                        _countDown.cancel();
+                        Intent i = new Intent(_ch.getContext(), Score.class);
+                        i.putExtra("timeLeft", (_timeLeft/1000));
+                        if (_currentLevel > 3) {
+                            i.putExtra("isFinished", true);
+                            startActivity(i);
+                        } else {
+                            i.putExtra("isFinished", false);
+                            startActivity(i);
+                            loadImgs();
+                            _clickhandler = new ClickHandler();
+                            startLevel();
+                        }
+                    }
+                    dialog.dismiss();
+                }
             };
-    		t.start();
-    		
+
+            //final MotionEvent fEvent = event;
+            final int eventX = (int) event.getX();
+            final int eventY = (int) event.getY();
+
+            // Run clickHandler in a seperate thread
+            Thread t = new Thread(new Runnable() {
+                private int eX = eventX;
+                private int eY = eventY;
+                public void run() {
+                    try {
+                        //Looper.prepare(); // No idea why, just have to use it
+                        Looper.prepareMainLooper();
+                        
+                        //pass event into clickhandler for checks
+                        _clickhandler.handleClick(eX, eY);
+
+                        // Send empty message to the handler
+                        h.sendEmptyMessage(0);
+                    } catch (Exception e) {
+                        Log.e("Clickhandler", "Error in Thread handleClick: " + e.getMessage());
+                    }
+                }
+            });
+            t.start();
         }
-       
+        return false;
+    }
+
+    private void loadImgs() {
+        //int currentLevel = lvlmngr.getNewLevel();
+        //lvlmngr.load(currentLevel); 
         
-    return false;
+        int img1 = getResources().getIdentifier("zdv_0" + _currentLevel + "_01","drawable","nl.hro.minor.android.lettergame.jjs");
+        int img2 = getResources().getIdentifier("zdv_0" + _currentLevel + "_02","drawable","nl.hro.minor.android.lettergame.jjs");
+        
+        bitmapCompare bmc = new bitmapCompare();
+        //Bitmap diffMap = bmc.getDiffMap(R.drawable.zdv_01_01, R.drawable.zdv_01_02);
+        Bitmap diffMap = bmc.getDiffMap(img1, img2);
+        	
+        _clickArea.setImageBitmap(diffMap);
+        _imageView1.setImageResource(img1);
+        _imageView2.setImageResource(img2);
     }
     
-    private void startLevel()
-    {
-    	int playtime = playTime*1000;
-    	countDown = new CountDownTimer(playtime, 1000) {
-
-    	     public void onTick(long millisUntilFinished) {
-    	    	 
-    	    	 float playtime = playTime*1000;
-    	    	 float time = (100/playtime) * (float) millisUntilFinished;
-    	    	 _timeLeft = (int)millisUntilFinished;
-    	    	 progessBar.setProgress((int)time);
-    	     }
-
-    	     public void onFinish() {
-    	    	 finishGame();
-    	     }
-    	     public void cancelTimer() {
-    	    	 this.cancel();
-    	     }
-    	  }.start();
-    	 
-    	  
-		//int currentLevel = lvlmngr.getNewLevel();
-		//lvlmngr.load(currentLevel); 
-		
-		int img1 = getResources().getIdentifier("zdv_0" + currentLevel + "_01","drawable","nl.hro.minor.android.lettergame.jjs");
-		int img2 = getResources().getIdentifier("zdv_0" + currentLevel + "_02","drawable","nl.hro.minor.android.lettergame.jjs");
-		
-		bitmapCompare bmc = new bitmapCompare();
-		//Bitmap diffMap = bmc.getDiffMap(R.drawable.zdv_01_01, R.drawable.zdv_01_02);
-		Bitmap diffMap = bmc.getDiffMap(img1, img2);
-			
-		clickArea.setImageBitmap(diffMap);
-		imageView1.setImageResource(img1);
-		imageView2.setImageResource(img2);
+    private void startLevel() {
+        int playtime = playTime*1000;
+        _countDown = new CountDownTimer(playtime, 1000) {
+        
+             public void onTick(long millisUntilFinished) {
+                 float playtime = playTime*1000;
+                 float time = (100/playtime) * (float) millisUntilFinished;
+                 _timeLeft = (int)millisUntilFinished;
+                 _progessBar.setProgress((int)time);
+             }
+        
+             public void onFinish() {
+            	 finishGame();
+             }
+             public void cancelTimer() {
+            	 this.cancel();
+             }
+          }.start();
     }
     
     private void finishGame() {
- 		Intent i = new Intent(ch.getContext(), Score.class);
-		i.putExtra("isFinished", true);
- 		startActivity(i);
+        Intent i = new Intent(_ch.getContext(), Score.class);
+        i.putExtra("isFinished", true);
+        startActivity(i);
     }
 }
